8ca15358817347bfa566466f2ba0691ea597a80d
junioradmin <Linus@freistetter.com>
2024-04-23 18:15:21 +0200

    gö

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 document.addEventListener("focus ", main);
 document.addEventListener("load", main);
 document.addEventListener("DOMContentLoaded", main);
-document.addEventListener("mousemove", main);
-function main(){
+function main() {
     document.getElementsByClassName("waiting-for-activity-text")[0].innerText = "Jetzt moch ma des Quizzerl, gö";
-    document.getElementsByClassName("hex-loading-container")[0].innerHTML = "<img src=\"https://raw.githubusercontent.com/LlordZombie/socrative-zainzify-mozilla/master/web-ext-artifacts/hz-48%20-%20Copy%20(Custom).png\">";
+    document.getElementsByClassName("hex-loading-container")[0].innerHTML = "<img id='spinnyboy' src=\"https://raw.githubusercontent.com/LlordZombie/socrative-zainzify-mozilla/master/web-ext-artifacts/hz-48%20-%20Copy%20(Custom).png\">";
 }
